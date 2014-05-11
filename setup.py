c0af245b91fb156119c30446561e1632f1fab351
shonenada <shonenada@gmail.com>
2014-05-11 23:27:22 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
 
-install_required = [l.strip() for l in open("requirements.txt", "r")]
-
-
-metadata = {'name': 'crawler',
-            'version': '0.1',
-            'packages': find_packages(),
-            'author': 'shonenada',
-            'author_email': 'shonenada@gmail.com',
-            'url': "https://github.com/shonenada/crawler",
-            'zip_safe': False,
-            'platforms': ['all'],
-            'install_required': install_required}
-
-if __name__ == '__main__':
-    setup(**metadata)
+setup(
+    name="simple-crawler",
+    version="0.1",
+    url="https://github.com/shonenada/crawler",
+    author="shonenada",
+    author_email="shonenada@gmail.com",
+    description="Simple crawler",
+    zip_safe=True,
+    platforms="any",
+    install_requires=["requests==2.2.1"],
+)
